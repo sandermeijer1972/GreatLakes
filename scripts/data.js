1b203ba8e0ad1091858ae7ffa8d3e281f9865fd0
Sander Meijer <sandertje1972@hotmail.com>
2022-07-30 08:51:11 +0200

    updated Soo Locks cam

--- scripts/data.js
+++ scripts/data.js
@@ -41,7 +41,7 @@ const camdata = [
         plaats: "Sault Ste Marie, Michigan",
         positie: "B",
         info: "De camera is bevestigd op de Tower of History, en geeft een uitzicht over de St Mary's River en de Soo Locks-sluizen.",
-        youtube: "https://www.youtube.com/embed/3AWBZSD9P-Y"
+        youtube: "https://www.youtube.com/embed/U6-IMHRLy2c"
     },
     {
         plaats: "Duluth, Minnesota",
